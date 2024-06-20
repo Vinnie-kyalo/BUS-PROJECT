3551e8008f1fd83ddf8f07062af1d04a96c18b35
vincent kyalo <147550634+Vinnie-kyalo@users.noreply.github.com>
2024-06-20 06:59:23 -0700

    Add files via upload

---
+++ scrach/vinnie.py
@@ -0,0 +1,11 @@
+student_name=input("enter your name: ")
+student_age=int(input("enter your age: "))
+if student_age <=20:
+    print("show id card")
+else:
+    print("pass")
+print(student_name)
+print(student_age)
+
+
+
